0d03bf06b2ce33a4149e90c1c77d9d9453b65223
Thanya113 <118598797+Thanya113@users.noreply.github.com>
2024-04-13 05:11:35 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faPhone, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 import brand from '../assets/company.png'; // Import your brand logo component here
 
 const Footer = () => {
@@ -18,29 +16,25 @@ const Footer = () => {
     <footer style={{ backgroundColor: '#f2f2f2', color: '#333' }}>
       <Container>
         {/* Copyright */}
-        
-        <Row className='justify-content-center'> {/* Center align content */}
+        <Row className='justify-content-center'>
           {/* Location */}
           <Col md={4} className='text-center py-3'>
             <div style={{ marginBottom: '10px' }}>
-              <FontAwesomeIcon icon={faMapMarkerAlt} style={{ marginRight: '10px' }} />
-              <b><span style={{ color: '#333' ,fontStyle:"italic"}}>Find Us</span></b>
+              📍 <b><span style={{ color: '#333' ,fontStyle:"italic"}}>Find Us</span></b>
               <p style={{marginTop:"7px"}}>Paramathi-Velur, Namakkal-637 207</p>
             </div>
           </Col>
           {/* Email */}
           <Col md={4} className='text-center py-3'>
             <div style={{ marginBottom: '10px' }}>
-              <FontAwesomeIcon icon={faEnvelope} style={{ marginRight: '10px' }} />
-              <b><a href="mailto:info@example.com" style={{ color: '#333', textDecoration: 'none',fontStyle:"italic" }}>Mail Us</a></b>
+              ✉️ <b><a href="mailto:info@example.com" style={{ color: '#333', textDecoration: 'none',fontStyle:"italic" }}>Mail Us</a></b>
               <p style={{marginTop:"7px"}}>srinavaladielectricals@gmail.com</p>
             </div>
           </Col>
           {/* Phone */}
           <Col md={4} className='text-center py-3'>
             <div style={{ marginBottom: '0px' }}>
-              <FontAwesomeIcon icon={faPhone} style={{ marginRight: '10px' }} />
-              <b><span style={{ color: '#333',fontStyle:"italic" }}>Call Us</span></b>
+              ☎️ <b><span style={{ color: '#333',fontStyle:"italic" }}>Call Us</span></b>
               <p style={{marginTop:"7px"}}>+91 9442251951</p>
             </div>
           </Col>
